a028b2a194f9e144babd1362f6a21513e548ffbe
rmesquit <rodrigo@mastria.com.br>
2014-12-20 15:08:50 -0500

    Update Adafruit_VS1053_Library.h

--- firmware/Adafruit_VS1053_Library.h
+++ firmware/Adafruit_VS1053_Library.h
@@ -15,9 +15,9 @@
 #define ADAFRUIT_VS1053_H
 
 #if defined(ARDUINO) && ARDUINO >= 100
- #include "Arduino.h'
+ #include "Arduino.h"
 #else
- #include "WProgram.h"
+// #include "WProgram.h"
  #include <pins_arduino.h>
 #endif
 
