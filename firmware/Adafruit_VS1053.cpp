afc060ec673c917e037c750f3b2d1ad19878014d
rmesquit <rodrigo@mastria.com.br>
2014-12-20 14:46:36 -0500

    Update Adafruit_VS1053.cpp

--- firmware/Adafruit_VS1053.cpp
+++ firmware/Adafruit_VS1053.cpp
@@ -12,8 +12,8 @@
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
-#include <firmware/Adafruit_VS1053.h>
-#include <SD.h>
+#include "firmware/Adafruit_VS1053.h"
+#include "SD.h"
 
 static Adafruit_VS1053_FilePlayer *myself;
 
