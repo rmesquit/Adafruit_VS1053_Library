3e13d353f5ef46dc9a878f2a454b571e09a6b37d
rmesquit <rodrigo@mastria.com.br>
2014-12-20 18:25:35 -0500

    Update Adafruit_VS1053_Library.h

--- firmware/Adafruit_VS1053_Library.h
+++ firmware/Adafruit_VS1053_Library.h
@@ -14,18 +14,12 @@
 #ifndef ADAFRUIT_VS1053_H
 #define ADAFRUIT_VS1053_H
 
-#if defined(ARDUINO) && ARDUINO >= 100
- #include "Arduino.h"
-#else
-// #include "WProgram.h"
 // #include <pins_arduino.h>
-#endif
 
 //#include "pins_arduino.h"
 //#include "wiring_private.h"
-//#include <SPI.h> 
-//#include <SD.h>
-//#include "sd-card-library.h"
+#include <SPI.h> 
+#include <SD.h>
 
 #define VS1053_FILEPLAYER_TIMER0_INT 255 // allows useInterrupt to accept pins 0 to 254
 #define VS1053_FILEPLAYER_PIN_INT 5
